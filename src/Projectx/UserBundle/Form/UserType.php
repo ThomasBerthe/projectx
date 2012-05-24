fa9878163ea4684740a07de3736e17a5523686b3
Thomas BERTHE <tomaou@macbook-pro-de-thomas-berthe.local>
2012-05-24 22:32:32 +0200

    Init

---
+++ src/Projectx/UserBundle/Form/UserType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Projectx\UserBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilder;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilder $builder, array $options)
+    {
+        $builder
+            ->add('username')
+            ->add('usernameCanonical')
+            ->add('email')
+            ->add('emailCanonical')
+            ->add('enabled')
+            ->add('salt')
+            ->add('password')
+            ->add('lastLogin')
+            ->add('locked')
+            ->add('expired')
+            ->add('expiresAt')
+            ->add('confirmationToken')
+            ->add('passwordRequestedAt')
+            ->add('roles')
+            ->add('credentialsExpired')
+            ->add('credentialsExpireAt')
+        ;
+    }
+
+    public function getName()
+    {
+        return 'projectx_userbundle_usertype';
+    }
+}
